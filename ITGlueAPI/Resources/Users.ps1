4a6f9e80f3f120c75236e6047df973ca9d3a9ad5
nonBinaryGeek <65196866+nonBinaryGeek@users.noreply.github.com>
2023-03-21 17:47:29 -0400

    Update Users.ps1

--- ITGlueAPI/Resources/Users.ps1
+++ ITGlueAPI/Resources/Users.ps1
@@ -77,6 +77,8 @@ function Set-ITGlueUsers {
 
     $body = ConvertTo-Json -InputObject $data -Depth $ITGlue_JSON_Conversion_Depth
 
+		$body = [System.Text.Encoding]::UTF8.GetBytes($body)
+
     try {
         $ITGlue_Headers.Add('x-api-key', (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'N/A', $ITGlue_API_Key).GetNetworkCredential().Password)
         $rest_output = Invoke-RestMethod -method 'PATCH' -uri ($ITGlue_Base_URI + $resource_uri) -headers $ITGlue_Headers `
