ec44322e887fd1d97d1009c97d65bb75a7e86143
nonBinaryGeek <65196866+nonBinaryGeek@users.noreply.github.com>
2023-03-21 17:40:14 -0400

    Update ContactTypes.ps1

--- ITGlueAPI/Resources/ContactTypes.ps1
+++ ITGlueAPI/Resources/ContactTypes.ps1
@@ -12,6 +12,8 @@ function New-ITGlueContactTypes {
 
     $body = ConvertTo-Json -InputObject $body -Depth $ITGlue_JSON_Conversion_Depth
 
+		$body = [System.Text.Encoding]::UTF8.GetBytes($body)
+
     try {
         $ITGlue_Headers.Add('x-api-key', (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'N/A', $ITGlue_API_Key).GetNetworkCredential().Password)
         $rest_output = Invoke-RestMethod -method 'POST' -uri ($ITGlue_Base_URI + $resource_uri) -headers $ITGlue_Headers `
@@ -98,6 +100,8 @@ function Set-ITGlueContactTypes {
     $body += @{'data'= $data}
 
     $body = ConvertTo-Json -InputObject $body -Depth $ITGlue_JSON_Conversion_Depth
+
+		$body = [System.Text.Encoding]::UTF8.GetBytes($body)
 
     try {
         $ITGlue_Headers.Add('x-api-key', (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'N/A', $ITGlue_API_Key).GetNetworkCredential().Password)
