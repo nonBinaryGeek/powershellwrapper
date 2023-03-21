49092467121252ceb7231f8f5c420a1faef778a5
nonBinaryGeek <65196866+nonBinaryGeek@users.noreply.github.com>
2023-03-21 17:44:09 -0400

    Update Models.ps1

--- ITGlueAPI/Resources/Models.ps1
+++ ITGlueAPI/Resources/Models.ps1
@@ -18,6 +18,8 @@ function New-ITGlueModels {
 
     $body = ConvertTo-Json -InputObject $body -Depth $ITGlue_JSON_Conversion_Depth
 
+		$body = [System.Text.Encoding]::UTF8.GetBytes($body)
+
     try {
         $ITGlue_Headers.Add('x-api-key', (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'N/A', $ITGlue_API_Key).GetNetworkCredential().Password)
         $rest_output = Invoke-RestMethod -method 'POST' -uri ($ITGlue_Base_URI + $resource_uri) -headers $ITGlue_Headers `
@@ -130,6 +132,8 @@ function Set-ITGlueModels {
     $body += @{'data' = $data}
 
     $body = ConvertTo-Json -InputObject $body -Depth $ITGlue_JSON_Conversion_Depth
+
+		$body = [System.Text.Encoding]::UTF8.GetBytes($body)
 
     try {
         $ITGlue_Headers.Add('x-api-key', (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'N/A', $ITGlue_API_Key).GetNetworkCredential().Password)
