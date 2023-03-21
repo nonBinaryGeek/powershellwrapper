a83f206df9db06c8600639d1add8fec08ec3e6ae
nonBinaryGeek <65196866+nonBinaryGeek@users.noreply.github.com>
2023-03-21 17:38:20 -0400

    Update Attachments.ps1

--- ITGlueAPI/Resources/Attachments.ps1
+++ ITGlueAPI/Resources/Attachments.ps1
@@ -21,6 +21,8 @@ function New-ITGlueAttachments {
 
     $body = ConvertTo-Json -InputObject $body -Depth $ITGlue_JSON_Conversion_Depth
 
+		$body = [System.Text.Encoding]::UTF8.GetBytes($body)
+
     try {
         $ITGlue_Headers.Add('x-api-key', (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'N/A', $ITGlue_API_Key).GetNetworkCredential().Password)
         $rest_output = Invoke-RestMethod -method 'POST' -uri ($ITGlue_Base_URI + $resource_uri) -headers $ITGlue_Headers `
@@ -62,6 +64,8 @@ function Set-ITGlueAttachments {
     $body += @{'data' = $data}
 
     $body = ConvertTo-Json -InputObject $body -Depth $ITGlue_JSON_Conversion_Depth
+
+		$body = [System.Text.Encoding]::UTF8.GetBytes($body)
 
     try {
         $ITGlue_Headers.Add('x-api-key', (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'N/A', $ITGlue_API_Key).GetNetworkCredential().Password)
