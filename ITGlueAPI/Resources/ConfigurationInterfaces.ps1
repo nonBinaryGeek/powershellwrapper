8e2d39308d0ffd051eb37118a9819acc20642e7b
nonBinaryGeek <65196866+nonBinaryGeek@users.noreply.github.com>
2023-03-21 18:26:48 -0400

    Update ConfigurationInterfaces.ps1

--- ITGlueAPI/Resources/ConfigurationInterfaces.ps1
+++ ITGlueAPI/Resources/ConfigurationInterfaces.ps1
@@ -23,7 +23,7 @@ function New-ITGlueConfigurationInterfaces {
     try {
         $ITGlue_Headers.Add('x-api-key', (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'N/A', $ITGlue_API_Key).GetNetworkCredential().Password)
         $rest_output = Invoke-RestMethod -method 'POST' -uri ($ITGlue_Base_URI + $resource_uri) -headers $ITGlue_Headers `
-        -body $body -ContentType "application/vnd.api+json; charset=utf-8" -ErrorAction Stop -ErrorVariable $web_error
+            -body $body -ContentType "application/vnd.api+json; charset=utf-8" -ErrorAction Stop -ErrorVariable $web_error
     } catch {
         Write-Error $_
     } finally {
@@ -149,7 +149,7 @@ function Set-ITGlueConfigurationInterfaces {
     try {
         $ITGlue_Headers.Add('x-api-key', (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'N/A', $ITGlue_API_Key).GetNetworkCredential().Password)
         $rest_output = Invoke-RestMethod -method 'PATCH' -uri ($ITGlue_Base_URI + $resource_uri) -headers $ITGlue_Headers `
-        -body $body -ContentType "application/vnd.api+json; charset=utf-8" -ErrorAction Stop -ErrorVariable $web_error
+            -body $body -ContentType "application/vnd.api+json; charset=utf-8" -ErrorAction Stop -ErrorVariable $web_error
     } catch {
         Write-Error $_
     } finally {
