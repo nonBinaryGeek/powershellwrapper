2aa5d9d48fac7134a8e2f4859bb83cb1277a5dbf
nonBinaryGeek <65196866+nonBinaryGeek@users.noreply.github.com>
2023-03-21 17:07:35 -0400

    Update FlexibleAssetTypes.ps1

--- ITGlueAPI/Resources/FlexibleAssetTypes.ps1
+++ ITGlueAPI/Resources/FlexibleAssetTypes.ps1
@@ -11,11 +11,13 @@ function New-ITGlueFlexibleAssetTypes {
     $body += @{'data'= $data}
 
     $body = ConvertTo-Json -InputObject $body -Depth $ITGlue_JSON_Conversion_Depth
+    
+    $body = [System.Text.Encoding]::UTF8.GetBytes($body)
 
     try {
         $ITGlue_Headers.Add('x-api-key', (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'N/A', $ITGlue_API_Key).GetNetworkCredential().Password)
         $rest_output = Invoke-RestMethod -method 'POST' -uri ($ITGlue_Base_URI + $resource_uri) -headers $ITGlue_Headers `
-            -body $body -ErrorAction Stop -ErrorVariable $web_error
+            -body $body -ContentType "application/vnd.api+json; charset=utf-8" -ErrorAction Stop -ErrorVariable $web_error
     } catch {
         Write-Error $_
     } finally {
@@ -121,10 +123,12 @@ function Set-ITGlueFlexibleAssetTypes {
 
     $body = ConvertTo-Json -InputObject $body -Depth $ITGlue_JSON_Conversion_Depth
 
+    $body = [System.Text.Encoding]::UTF8.GetBytes($body)
+
     try {
         $ITGlue_Headers.Add('x-api-key', (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'N/A', $ITGlue_API_Key).GetNetworkCredential().Password)
         $rest_output = Invoke-RestMethod -method 'PATCH' -uri ($ITGlue_Base_URI + $resource_uri) -headers $ITGlue_Headers `
-            -body $body -ErrorAction Stop -ErrorVariable $web_error
+            -body $body -ContentType "application/vnd.api+json; charset=utf-8" -ErrorAction Stop -ErrorVariable $web_error
     } catch {
         Write-Error $_
     } finally {
