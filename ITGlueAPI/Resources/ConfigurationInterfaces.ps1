144a946719a73ad1615f62344d91edc09363d326
nonBinaryGeek <65196866+nonBinaryGeek@users.noreply.github.com>
2023-03-21 17:38:50 -0400

    Update ConfigurationInterfaces.ps1

--- ITGlueAPI/Resources/ConfigurationInterfaces.ps1
+++ ITGlueAPI/Resources/ConfigurationInterfaces.ps1
@@ -18,6 +18,8 @@ function New-ITGlueConfigurationInterfaces {
 
     $body = ConvertTo-Json -InputObject $body -Depth $ITGlue_JSON_Conversion_Depth
 
+		$body = [System.Text.Encoding]::UTF8.GetBytes($body)
+
     try {
         $ITGlue_Headers.Add('x-api-key', (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'N/A', $ITGlue_API_Key).GetNetworkCredential().Password)
         $rest_output = Invoke-RestMethod -method 'POST' -uri ($ITGlue_Base_URI + $resource_uri) -headers $ITGlue_Headers `
@@ -141,6 +143,8 @@ function Set-ITGlueConfigurationInterfaces {
     $body += @{'data' = $data}
 
     $body = ConvertTo-Json -InputObject $body -Depth $ITGlue_JSON_Conversion_Depth
+
+		$body = [System.Text.Encoding]::UTF8.GetBytes($body)
 
     try {
         $ITGlue_Headers.Add('x-api-key', (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'N/A', $ITGlue_API_Key).GetNetworkCredential().Password)
